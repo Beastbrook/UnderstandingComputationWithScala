2bd43f9923425228d3aa27eb543f4ca7f348a313
yukity <heyawoakarukusite@gmail.com>
2014-10-10 16:32:16 +0900

    Char -> Option[Char]

--- src/main/scala/chapter03/NFA.scala
+++ src/main/scala/chapter03/NFA.scala
@@ -1,35 +1,35 @@
 package uc.nfa
 
 sealed trait Rule[A] {
-  def canApplyTo(s: A, ch: Char): Boolean
+  def canApplyTo(s: A, ch: Option[Char]): Boolean
   def follow: Option[A]
 }
-case class FARule[A](state: A, character: Char, nextState: A) extends Rule[A] {
-  override def canApplyTo(s: A, ch: Char): Boolean = state == s && character == ch
+case class FARule[A](state: A, character: Option[Char], nextState: A) extends Rule[A] {
+  override def canApplyTo(s: A, ch: Option[Char]): Boolean = state == s && character == ch
   override def follow: Option[A] = Some(nextState)
-  override def toString: String = s"FARule<${state} -- ${character} --> ${nextState}>"
+  override def toString: String = s"FARule<${state} -- ${character.getOrElse(' ')} --> ${nextState}>"
 }
 case class NoRule[A]() extends Rule[A] {
   override def toString: String = "No Rule"
   override def follow: Option[A] = None
-  override def canApplyTo(s: A, ch: Char): Boolean = false
+  override def canApplyTo(s: A, ch: Option[Char]): Boolean = false
 }
 
 case class NFARulebook[A](rules: Set[Rule[A]]) {
-  def nextStates(states: Set[A], character: Char): Set[A] =
+  def nextStates(states: Set[A], character: Option[Char]): Set[A] =
     states.flatMap( followRulesFor(_, character) )
       .filter( _ != None )
       .map ( _.get )
-  def followRulesFor(state: A, character: Char): Set[Option[A]] = rulesFor(state, character).map( _.follow )
-  def rulesFor(state: A, character: Char): Set[Rule[A]] = rules.filter( _.canApplyTo(state, character) )
+  def followRulesFor(state: A, character: Option[Char]): Set[Option[A]] = rulesFor(state, character).map( _.follow )
+  def rulesFor(state: A, character: Option[Char]): Set[Rule[A]] = rules.filter( _.canApplyTo(state, character) )
 }
 
 case class NFA[A](currentStates: Set[A], acceptStates: Set[A], rulebook: NFARulebook[A]) {
   def isAccepting: Boolean = (currentStates & acceptStates).size != 0
-  def readCharacter(character: Char): NFA[A] =
+  def readCharacter(character: Option[Char]): NFA[A] =
     NFA(rulebook.nextStates(currentStates, character), acceptStates, rulebook)
   def readString(string: String): NFA[A] =
     if (string == "") this
-    else readCharacter(string.head).readString(string.tail)
+    else readCharacter(Some(string.head)).readString(string.tail)
   def acceptString(string: String): Boolean = readString(string).isAccepting
 }
--- src/test/scala/chapter03/NFASpec.scala
+++ src/test/scala/chapter03/NFASpec.scala
@@ -6,21 +6,21 @@ class NFASpec extends FlatSpec with Matchers {
 
   "NFARulebook#nextStates" should "return set of state" in {
     val rulebook: NFARulebook[Int] = NFARulebook(Set(
-      FARule(1, 'A', 2),
-      FARule(2, 'B', 3),
-      FARule(3, 'C', 1)
+      FARule(1, Some('A'), 2),
+      FARule(2, Some('B'), 3),
+      FARule(3, Some('C'), 1)
     ))
-    val nextStates: Set[Int] = rulebook.nextStates(Set(1, 2), 'B')
+    val nextStates: Set[Int] = rulebook.nextStates(Set(1, 2), Some('B'))
     nextStates should be (Set(3))
-    val nextStates2: Set[Int] = rulebook.nextStates(Set(1), 'C')
+    val nextStates2: Set[Int] = rulebook.nextStates(Set(1), Some('C'))
     nextStates2 should be (Set())
   }
 
   "NFA#isAccepting" should "return acception or not" in {
     val rulebook: NFARulebook[Int] = NFARulebook(Set(
-      FARule(1, 'A', 2),
-      FARule(2, 'B', 3),
-      FARule(3, 'C', 1)
+      FARule(1, Some('A'), 2),
+      FARule(2, Some('B'), 3),
+      FARule(3, Some('C'), 1)
     ))
     val nfa: NFA[Int] = NFA(Set(1, 2), Set(2), rulebook)
     nfa.isAccepting should be (true)
@@ -30,20 +30,20 @@ class NFASpec extends FlatSpec with Matchers {
 
   "NFA#readCharacter" should "return NFA" in {
     val rulebook: NFARulebook[Int] = NFARulebook(Set(
-      FARule(1, 'A', 2),
-      FARule(2, 'B', 3),
-      FARule(3, 'C', 1)
+      FARule(1, Some('A'), 2),
+      FARule(2, Some('B'), 3),
+      FARule(3, Some('C'), 1)
     ))
     val nfa: NFA[Int] = NFA(Set(1, 2), Set(2), rulebook)
-    nfa.readCharacter('B') should be (NFA(Set(3), Set(2), rulebook))
+    nfa.readCharacter(Some('B')) should be (NFA(Set(3), Set(2), rulebook))
   }
 
   "NFA#readString" should "return NFA" in {
     val rulebook: NFARulebook[Int] = NFARulebook(Set(
-      FARule(1, 'A', 2),
-      FARule(1, 'B', 1),
-      FARule(2, 'B', 3),
-      FARule(3, 'C', 1)
+      FARule(1, Some('A'), 2),
+      FARule(1, Some('B'), 1),
+      FARule(2, Some('B'), 3),
+      FARule(3, Some('C'), 1)
     ))
     val nfa: NFA[Int] = NFA(Set(1, 2), Set(2), rulebook)
     nfa.readString("A") should be (NFA(Set(2), Set(2), rulebook))
@@ -53,10 +53,10 @@ class NFASpec extends FlatSpec with Matchers {
 
   "NFA#acceptString" should "return if the string is acceptable" in {
     val rulebook: NFARulebook[Int] = NFARulebook(Set(
-      FARule(1, 'A', 2),
-      FARule(1, 'B', 1),
-      FARule(2, 'B', 3),
-      FARule(3, 'C', 1)
+      FARule(1, Some('A'), 2),
+      FARule(1, Some('B'), 1),
+      FARule(2, Some('B'), 3),
+      FARule(3, Some('C'), 1)
     ))
     val nfa: NFA[Int] = NFA(Set(1, 2), Set(2), rulebook)
     nfa.acceptString("BBA") should be (true)
