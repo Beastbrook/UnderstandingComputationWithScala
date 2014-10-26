48bee86ddc43c24220d5a164bd57f98549cadc2c
yukity <heyawoakarukusite@gmail.com>
2014-10-26 17:45:26 +0900

    DTMRulebook#nextConfiguration added.

--- src/main/scala/chapter05/DTMRulebook.scala
+++ src/main/scala/chapter05/DTMRulebook.scala
@@ -3,4 +3,6 @@ package uc.tm
 case class DTMRulebook[A,B](rules: Set[TMRule[A,B]]) {
   def ruleFor(configuration: TMConfiguration[A,B]): TMRule[A,B] =
     rules.find( _.appliesTo(configuration) ).get
+  def nextConfiguration(configuration: TMConfiguration[A,B]): TMConfiguration[A,B] =
+    ruleFor(configuration).follow(configuration)
 }
--- src/test/scala/chapter05/DTMRulebookSpec.scala
+++ src/test/scala/chapter05/DTMRulebookSpec.scala
@@ -7,15 +7,21 @@ class DTMRulebookSpec extends FlatSpec with Matchers {
   // increment binary dicimal
   val rulebook: DTMRulebook[Int,Char] = DTMRulebook(Set(
     TMRule(1, Some('0'), 2, Some('1'), RIGHT),
-    TMRule(1, Some('1'), 1, Some('1'), LEFT),
+    TMRule(1, Some('1'), 1, Some('0'), LEFT),
     TMRule(1, Some('_'), 2, Some('1'), RIGHT),
-    TMRule(2, Some('0'), 2, Some('1'), RIGHT),
+    TMRule(2, Some('0'), 2, Some('0'), RIGHT),
     TMRule(2, Some('1'), 2, Some('1'), RIGHT),
-    TMRule(2, Some('_'), 3, Some('1'), LEFT)
+    TMRule(2, Some('_'), 3, Some('_'), LEFT)
   ))
   val start: TMConfiguration[Int,Char] = TMConfiguration(1, Tape(List('1', '0', '1'), '1', List[Char](), '_'))
 
   "ruleFor" should "search appliable rule" in {
-    rulebook.ruleFor(start) should be (TMRule(1, Some('1'), 1, Some('1'), LEFT))
+    rulebook.ruleFor(start) should be (TMRule(1, Some('1'), 1, Some('0'), LEFT))
   }
+
+  "nextConfiguration" should "return next configuration that follow rules" in {
+    rulebook.nextConfiguration(start) should be (
+      TMConfiguration(1, Tape(List('1', '0'), '1', List('0'), '_')))
+  }
+
 }
