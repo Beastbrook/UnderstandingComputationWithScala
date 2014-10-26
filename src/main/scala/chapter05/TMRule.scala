639194d7ed2722cb73922e98d8402badca1faa3b
yukity <heyawoakarukusite@gmail.com>
2014-10-26 15:05:58 +0900

    rule's direction fixed

--- src/main/scala/chapter05/TMRule.scala
+++ src/main/scala/chapter05/TMRule.scala
@@ -2,13 +2,21 @@ package uc.tm
 
 case class TMConfiguration[A,B](state: A, tape: Tape[B])
 
+sealed trait Direction
+case object LEFT extends Direction
+case object RIGHT extends Direction
+
 case class TMRule[A,B](
   state: A,
   character: Option[B],
   nextState: A,
   writeCharacter: Option[B],
-  direction: String) {
+  direction: Direction) {
 
   def appliesTo(configuration: TMConfiguration[A,B]) =
     state == configuration.state && character == Some(configuration.tape.middle)
+  def nextTape(configuration: TMConfiguration[A,B]): Tape[B] = direction match {
+    case LEFT => configuration.tape.write(writeCharacter.get).moveHeadLeft
+    case RIGHT => configuration.tape.write(writeCharacter.get).moveHeadRight
+  }
 }
--- src/test/scala/chapter05/TMRuleSpec.scala
+++ src/test/scala/chapter05/TMRuleSpec.scala
@@ -4,12 +4,16 @@ import org.scalatest._
 
 class TMRuleSpec extends FlatSpec with Matchers {
 
-  val rule: TMRule[Int,Char] = TMRule(1, Some('0'), 2, Some('1'), "RIGHT")
+  val rule: TMRule[Int,Char] = TMRule(1, Some('0'), 2, Some('1'), RIGHT)
 
   "appliesTo" should "return if appliable to configuration or not" in {
     rule.appliesTo(TMConfiguration(1, Tape(List[Char](), '0', List[Char](), '_'))) should be (true)
     rule.appliesTo(TMConfiguration(1, Tape(List[Char](), '1', List[Char](), '_'))) should be (false)
     rule.appliesTo(TMConfiguration(2, Tape(List[Char](), '0', List[Char](), '_'))) should be (false)
+  }
+
+  "nextTape" should "return next tape that follow the rule" in {
+    rule.nextTape(TMConfiguration(1, Tape(List[Char](), '0', List[Char](), '_'))).toString should be ("#<tape 1(_)>")
   }
 
 }
