a03896c16b93435a5c0c80c9725e8ec820de8de6
yukity <heyawoakarukusite@gmail.com>
2014-11-07 20:17:03 +0900

    Refactor FizzBuzz make

--- src/main/scala/chapter06/FizzBuzz.scala
+++ src/main/scala/chapter06/FizzBuzz.scala
@@ -4,13 +4,15 @@ object FizzBuzz {
 
   // Main fizzbuzz program
   def make: List[String] =
-    (0 to 100).toList
-      .map {
-        case n if n % 15 == 0 => "FizzBuzz"
-        case n if n % 3  == 0 => "Fizz"
-        case n if n % 5  == 0 => "Buzz"
-        case n => n.toString
-      }
+    (toInt(ZERO) to toInt(HANDRED))
+      .toList
+      .map((n: Int) => {
+        IF(toFBBool(n % 15 == 0))("FizzBuzz")(
+          IF(toFBBool(n % 3  == 0))("Fizz")(
+            IF(toFBBool(n % 5  == 0))("Buzz")(n.toString)
+          )
+        ).asInstanceOf[String]
+      })
 
   // type definition
   type FBInt  = (Int => Int) => (Int => Int)
@@ -60,5 +62,7 @@ object FizzBuzz {
     f(_ + 1)(0)
   def toBoolean(f: Any => Any => Any): Boolean =
     f(true)(false).asInstanceOf[Boolean]
+  def toFBBool(b: Boolean): FBBool =
+    if (b) TRUE else FALSE
 
 }
