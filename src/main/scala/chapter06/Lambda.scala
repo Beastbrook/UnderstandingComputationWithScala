fc2b5e2089358010470ff54122eaeb1609253247
yukity <heyawoakarukusite@gmail.com>
2014-11-08 15:25:30 +0900

    separate FizzBuzz and Lambda

---
+++ src/main/scala/chapter06/Lambda.scala
@@ -0,0 +1,82 @@
+package uc.lambda
+
+object Lambda {
+
+  // type definition
+  type FBInt  = (Any => Any) => (Any => Any)
+  type FBBool = Any => Any => Any
+  type FBPair = (Any => Any => Any) => Any
+
+  // Number
+  val ZERO: FBInt    = (p: (Any => Any)) => { (x: Any) => x }
+  val ONE: FBInt     = (p: (Any => Any)) => { (x: Any) => p(x) }
+  val TWO: FBInt     = (p: (Any => Any)) => { (x: Any) => p(p(x)) }
+  val THREE: FBInt   = (p: (Any => Any)) => { (x: Any) => p(p(p(x))) }
+  val FIVE: FBInt    = (p: (Any => Any)) => { (x: Any) => p(p(p(p(p(x))))) }
+  val FIFTEEN: FBInt = (p: (Any => Any)) => { (x: Any) => p(p(p(p(p(p(p(p(p(p(p(p(p(p(p(x))))))))))))))) }
+  val HANDRED: FBInt = (p: (Any => Any)) => { (x: Any) =>
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+    p(p(p(p(p(p(p(p(p(p(
+      x
+    ))))))))))
+    ))))))))))
+    ))))))))))
+    ))))))))))
+    ))))))))))
+    ))))))))))
+    ))))))))))
+    ))))))))))
+    ))))))))))
+    ))))))))))
+  }
+  val SLIDE: Any => Any =
+    (p: Any) => PAIR(RIGHT(p.asInstanceOf[FBPair]))(INCREMENT(RIGHT(p.asInstanceOf[FBPair]).asInstanceOf[FBInt]))
+  val INCREMENT: FBInt => FBInt =
+    (n: FBInt) => {
+      (p: Any => Any) => {
+        (x: Any) => p(n(p)(x))
+      }
+    }
+  val DECREMENT: FBInt => FBInt =
+    (n: FBInt) => LEFT( n(SLIDE)(PAIR(ZERO)(ZERO)).asInstanceOf[FBPair] ).asInstanceOf[FBInt]
+
+  // Boolean
+  val TRUE: FBBool  = (x: Any) => { (y: Any) => { x } }
+  val FALSE: FBBool = (x: Any) => { (y: Any) => { y } }
+
+  // Selection
+  val IF: FBBool => Any => Any => Any =
+    (bool: FBBool) => bool
+
+  val IS_ZERO: FBInt => FBBool =
+    (n: FBInt) => n(_ => (FALSE))(TRUE).asInstanceOf[FBBool]
+
+  // pair
+  val PAIR: Any => Any => FBPair =
+    (x: Any) => {
+      (y: Any) => {
+        (f: Any => Any => Any) => f(x)(y)
+      }
+    }
+  val LEFT: FBPair => Any =
+    (p: FBPair) => p((x: Any) => {(y: Any) => x})
+  val RIGHT: FBPair => Any =
+    (p: FBPair) => p((x: Any) => {(y: Any) => y})
+
+  // converters
+  def toInt(f: FBInt): Int =
+    f(_.asInstanceOf[Int] + 1)(0).asInstanceOf[Int]
+  def toBoolean(f: Any => Any => Any): Boolean =
+    f(true)(false).asInstanceOf[Boolean]
+  def toFBBool(b: Boolean): FBBool =
+    if (b) TRUE else FALSE
+
+}
---
+++ src/test/scala/chapter06/LambdaSpec.scala
@@ -0,0 +1,53 @@
+package uc.lambda
+
+import org.scalatest._
+
+class LambdaSpec extends FlatSpec with Matchers {
+
+  import Lambda._
+
+  "ONE,TWO,THREE" should "be able to translate to number" in {
+    toInt(ZERO) should be (0)
+    toInt(ONE) should be (1)
+    toInt(TWO) should be (2)
+    toInt(THREE) should be (3)
+    toInt(FIVE) should be (5)
+    toInt(FIFTEEN) should be (15)
+    toInt(HANDRED) should be (100)
+  }
+
+  "TRUE,FALSE" should "be able to translate to boolean" in {
+    toBoolean(TRUE) should be (true)
+    toBoolean(FALSE) should be (false)
+  }
+
+  "IF" should "work like if" in {
+    toInt(IF(TRUE)(ONE)(TWO).asInstanceOf[FBInt]) should be (1)
+    toInt(IF(FALSE)(ONE)(TWO).asInstanceOf[FBInt]) should be (2)
+    IF(TRUE)("happy")("sad").asInstanceOf[String] should be ("happy")
+    IF(FALSE)("happy")("sad").asInstanceOf[String] should be ("sad")
+  }
+
+  "IS_ZERO" should "return true if n == 0" in {
+    toBoolean(IS_ZERO(ZERO)) should be (true)
+    toBoolean(IS_ZERO(ONE)) should be (false)
+  }
+
+  "PAIR" should "have two values" in {
+    val myPair = PAIR(THREE)(FIVE)
+    toInt(LEFT(myPair).asInstanceOf[FBInt]) should be (3)
+    toInt(RIGHT(myPair).asInstanceOf[FBInt]) should be (5)
+  }
+
+  "INCREMENT" should "create n+1" in {
+    toInt(INCREMENT(ZERO)) should be (1)
+    toInt(INCREMENT(INCREMENT(ZERO))) should be (2)
+    toInt(INCREMENT(HANDRED)) should be (101)
+  }
+
+  "DECREMENT" should "create n-1" in {
+    toInt(DECREMENT(ONE)) should be (0)
+    toInt(DECREMENT(HANDRED)) should be (99)
+  }
+
+}
