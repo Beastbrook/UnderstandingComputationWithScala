9ca7b124b366ca1a233388c4e5054a08a333cf9c
yukity <heyawoakarukusite@gmail.com>
2014-11-14 21:50:04 +0900

    LC#toString make Ruby lambda

--- src/main/scala/chapter06/LC.scala
+++ src/main/scala/chapter06/LC.scala
@@ -1,2 +1,16 @@
 package uc.lambda.lc
 
+
+sealed trait LC
+case class LCVariable(name: String) extends LC {
+  override def toString: String =
+    name.toString
+}
+case class LCFunction(parameter: String, body: LC) extends LC {
+  override def toString: String =
+    s"-> ${parameter} { ${body} }"
+}
+case class LCCall(left: LC, right: LC) extends LC {
+  override def toString: String =
+    s"${left}[${right}]"
+}
--- src/test/scala/chapter06/LCSpec.scala
+++ src/test/scala/chapter06/LCSpec.scala
@@ -4,4 +4,40 @@ import org.scalatest._
 
 class LCSpec extends FlatSpec with Matchers {
 
+  "LC" should "convert to Ruby lambda" in {
+    val one = LCFunction("p",
+      LCFunction("x",
+        LCCall(LCVariable("p"), LCVariable("x"))
+      )
+    )
+    one.toString should be ("-> p { -> x { p[x] } }")
+
+    val increment =
+      LCFunction("n",
+        LCFunction("p",
+          LCFunction("x",
+            LCCall(
+              LCVariable("p"),
+              LCCall(
+                LCCall(LCVariable("n"), LCVariable("p")),
+                LCVariable("x")
+              )
+            )
+          )
+        )
+      )
+    increment.toString should be ("-> n { -> p { -> x { p[n[p][x]] } } }")
+
+    val add =
+      LCFunction("m",
+        LCFunction("n",
+          LCCall(
+            LCCall(LCVariable("n"), increment),
+            LCVariable("m")
+          )
+        )
+      )
+    add.toString should be ("-> m { -> n { n[-> n { -> p { -> x { p[n[p][x]] } } }][m] } }")
+  }
+
 }
