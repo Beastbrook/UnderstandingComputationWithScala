3a8541ed0749395fb3ffabeade9116c5865b59a8
yukity <heyawoakarukusite@gmail.com>
2014-11-24 21:18:23 +0900

    LC replace variable with body

--- src/main/scala/chapter06/LC.scala
+++ src/main/scala/chapter06/LC.scala
@@ -1,16 +1,26 @@
 package uc.lambda.lc
 
 
-sealed trait LC
+sealed trait LC {
+  def replace(name: String, replacement: LC): LC
+}
 case class LCVariable(name: String) extends LC {
   override def toString: String =
     name.toString
+  def replace(name: String, replacement: LC): LC =
+    if (this.name == name) replacement
+    else this
 }
 case class LCFunction(parameter: String, body: LC) extends LC {
   override def toString: String =
     s"-> ${parameter} { ${body} }"
+  override def replace(name: String, replacement: LC): LC =
+    if (parameter == name) this
+    else LCFunction(parameter, body.replace(name, replacement))
 }
 case class LCCall(left: LC, right: LC) extends LC {
   override def toString: String =
     s"${left}[${right}]"
+  def replace(name: String, replacement: LC): LC =
+    LCCall(left.replace(name, replacement), right.replace(name, replacement))
 }
--- src/test/scala/chapter06/LCSpec.scala
+++ src/test/scala/chapter06/LCSpec.scala
@@ -40,4 +40,37 @@ class LCSpec extends FlatSpec with Matchers {
     add.toString should be ("-> m { -> n { n[-> n { -> p { -> x { p[n[p][x]] } } }][m] } }")
   }
 
+  "LC" should "replace variable with body" in {
+    val expression1: LC = LCVariable("x")
+    expression1.replace("x", LCFunction("y", LCVariable("y"))).toString should be ("-> y { y }")
+    expression1.replace("z", LCFunction("y", LCVariable("y"))).toString should be ("x")
+
+    val expression2: LC = LCCall(
+      LCCall(
+        LCCall(
+          LCVariable("a"),
+          LCVariable("b")
+        ),
+        LCVariable("c")
+      ),
+      LCVariable("b")
+    )
+    expression2.toString should be ("a[b][c][b]")
+    expression2.replace("a", LCVariable("x")).toString should be ("x[b][c][b]")
+    expression2.replace("b", LCFunction("x", LCVariable("x"))).toString should be ("a[-> x { x }][c][-> x { x }]")
+
+    val expression3: LC = LCFunction("y", LCCall(LCVariable("x"), LCVariable("y")))
+    expression3.toString should be ("-> y { x[y] }")
+    expression3.replace("x", LCVariable("z")).toString should be ("-> y { z[y] }")
+    expression3.replace("y", LCVariable("z")).toString should be ("-> y { x[y] }")
+
+    val expression4: LC = LCCall(
+      LCCall(LCVariable("x"), LCVariable("y")),
+      LCFunction("y", LCCall(LCVariable("y"), LCVariable("x")))
+    )
+    expression4.toString should be ("x[y][-> y { y[x] }]")
+    expression4.replace("x", LCVariable("z")).toString should be ("z[y][-> y { y[z] }]")
+    expression4.replace("y", LCVariable("z")).toString should be ("x[z][-> y { y[x] }]")
+  }
+
 }
